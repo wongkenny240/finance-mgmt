a89afa01b31255fa66ce788dc4dbbcef658e093c
Kenny Wong <ckwongkennyw@gmail.com>
2020-10-15 10:16:18 +0800

    Update fetch_market_data.py

--- fetch_market_data.py
+++ fetch_market_data.py
@@ -1,17 +1,16 @@
 """
 Author: KCKW
 Description: fetch finance data for pandas dataframe and write to Excel
-
 """
 
 import pandas_datareader.data as web
-import matplotlib.pyplot as plt
 import pandas as pd
-import datetime
+from openpyxl import load_workbook
+
 
 if __name__ == '__main__':
     # Define the instruments to download. We would like to see Apple, Microsoft and the S&P500 index.
-    tickers = ['AAPL', 'AMZN']
+    tickers = ['1617.HK', '2289.HK']
     # We would like all available data from 01/01/2000 until 12/31/2016.
     start_date = '1980-01-01'
     end_date = '2020-10-05'
@@ -19,4 +18,28 @@ if __name__ == '__main__':
     for ticker in tickers:
         # User pandas_reader.data.DataReader to load the desired data. As simple as that.
         df = web.DataReader(ticker, 'yahoo', start_date, end_date)
-        print(df)
+        df = df.reset_index()
+        df['Date'] = df['Date'].dt.date
+        print(df.columns)
+
+        # write to excel
+        writer = pd.ExcelWriter("stock.xlsx", engine='openpyxl')
+        book = load_workbook("stock.xlsx")
+
+        # delete sheet
+        delete_sheet = [ticker]
+        for sheetName in book.sheetnames:
+            if sheetName in delete_sheet:
+                del book[sheetName]
+        book.save("stock.xlsx")
+
+        # set write parameter
+        writer.book = book
+        writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+
+        # output to excel
+        df.to_excel(writer,
+                          sheet_name=ticker, index=False)
+
+        writer.save()
+        writer.close()
