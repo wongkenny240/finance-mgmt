4c74bb2b69d9da122cc603e2ce72ce50ddc8d1c2
Kenny Wong <ckwongkennyw@gmail.com>
2020-10-15 10:16:40 +0800

    Update fetch_market_data.py

--- fetch_market_data.py
+++ fetch_market_data.py
@@ -10,7 +10,7 @@ from openpyxl import load_workbook
 
 if __name__ == '__main__':
     # Define the instruments to download. We would like to see Apple, Microsoft and the S&P500 index.
-    tickers = ['1617.HK', '2289.HK']
+    tickers = ['AAPL', 'AMZN']
     # We would like all available data from 01/01/2000 until 12/31/2016.
     start_date = '1980-01-01'
     end_date = '2020-10-05'
